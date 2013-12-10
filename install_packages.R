edc08a96ea882462d37ff20b2d41657d80142ca6
Alex Zvoleff <azvoleff@outlook.com>
2013-12-10 08:48:49 -0500

    Add colorout and vimcom.plus.

--- install_packages.R
+++ install_packages.R
@@ -3,3 +3,15 @@ pkgs <- as.character(pkgs$V1)
 install.packages(pkgs, repos="http://cran.revolutionanalytics.com")
 source("http://bioconductor.org/biocLite.R")
 biocLite("rhdf5")
+
+if (.Platform$OS.type == "unix") {
+    download.file("http://www.lepem.ufc.br/jaa/colorout_1.0-1.tar.gz", destfile = "colorout_1.0-1.tar.gz")
+    install.packages("colorout_1.0-1.tar.gz", type = "source", repos = NULL)
+}
+
+detach("package:vimcom.plus", unload=TRUE)
+download.file("http://www.lepem.ufc.br/jaa/vimcom.plus_0.9-92.zip",
+              destfile = "vimcom.plus_0.9-92.zip")
+install.packages("vimcom.plus_0.9-92.zip", type = "win.binary",
+                 repos = NULL)
+library(vimcom.plus)
